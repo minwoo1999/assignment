386770a4f3d7f6b2f0c21b8ffdcbe0f64077aa11
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2024-06-11 20:54:02 +0900

    Initial Commit

---
+++ src/main/java/com/example/flowglobalassignment/FlowGlobalAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowglobalassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowGlobalAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowGlobalAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flowglobalassignment/FlowGlobalAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowglobalassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowGlobalAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
